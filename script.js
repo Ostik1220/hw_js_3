ea3ee518e2dcd3a42c4f544a1dfbd091eaa1df87
Ostik <117195748+Ostik1220@users.noreply.github.com>
2024-10-04 15:58:55 +0300

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+const result = 5 + 5 + '5'
+console.log(typeof result)
+
+const email = "ostap.drymalyk@gmail.com"
+const mavpa = email.includes("@");
+console.log(mavpa)
+
+let fullName = "My name is"
+console.log (fullName + " Viktor")
+
+const userName = "Тетяна"
+const uWas = "Privat bank - head of the bank branch"
+
+console.log("з  лис 2014 р. - лип 2016 р. " + userName + " працювали як працівниця " + uWas)
